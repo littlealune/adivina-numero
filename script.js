1b3165fd34a8b3206fbc968c39d08b31b00a30d5
Little Alune <sara7915zuera@gmail.com>
2024-01-15 13:07:03 +0100

    Inicio de desarrollo del programa. TO DO init()

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
 
+const messageField = document.querySelector('.message')
+const scoreField = document.querySelector('.score')
+const highScoreField = document.querySelector('.highscore')
+const secretNumberField = document.querySelector('.number')
+const checkButton = document.querySelector('.check')
+const againButton = document.querySelector('.again')
+const guessNumberField = document.querySelector('.guess')
+
+initProgram()
+
+messageField.textContent = mensajeInicial
+scoreField.textContent = score
+highScoreField.textContent = highScore
+secretNumberField.textContent = '?'
+
+function initProgram ()  {
+    const mensajeInicial = 'Empieza a adivinar...'
+    let score = 20
+    let highScore = highScoreField ?? 0
+    const secretNumber = Math.trunc(Math.random() * 20) +1
+}
+
+checkButton.addEventListener('click',() => {
+    const guessNumber = Number(guessNumberField.value)
+    if(guessNumber > secretNumber){
+        score--
+        scoreField.textContent = score
+        messageField.textContent = 'Demasiado grande'
+    }else if (guessNumber < secretNumber){
+        score--
+        scoreField.textContent = score
+        messageField.textContent = 'Demasiado pequeño'
+    }else{
+        secretNumberField.textContent = secretNumber
+        document.body.style.backgroundColor = '#60b347'
+        checkButton.disabled = true
+        messageField.textContent = '¡Has acertado!'
+        if(highScore < score){
+            highScore = score
+            highScoreField.textContent = highScore
+        }
+    }
+})
+
+againButton.addEventListener('click', () => {
+    
+    secretNumberField.textContent = '?'
+    document.body.style.backgroundColor = '#222'
+    checkButton.disabled = false
+    messageField.textContent = 'Empieza a adivinar...'
+    guessNumberField.value = ''
+})
